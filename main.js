a2f19289a4205c265a864490cff5bf066d3d66f5
Jamie Mason <jamie@foldleft.io>
2019-02-07 21:00:04 +0000

    feat(app): display astexplorer UI in electron

---
+++ main.js
@@ -0,0 +1,34 @@
+const electron = require('electron');
+
+const { app, BrowserWindow } = electron;
+let win;
+
+const createWindow = () => {
+  const { height, width } = electron.screen.getPrimaryDisplay().workAreaSize;
+
+  win = new BrowserWindow({ center: true, height, width });
+  win.loadFile('./website/index.html');
+  win.webContents.openDevTools();
+
+  win.webContents.on('did-finish-load', () => {
+    console.log('READY');
+  });
+
+  win.on('closed', () => {
+    win = null;
+  });
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
