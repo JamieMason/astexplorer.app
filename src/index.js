e3e0d2dbee4fc1a32b864d7d6ed5354185da64fb
Jamie Mason <jamie@foldleft.io>
2019-09-22 23:19:16 +0100

    chore(app): only log to console in development

--- src/index.js
+++ src/index.js
@@ -3,21 +3,23 @@ const electron = require('electron');
 const log = require('./main/log');
 const { browserEvents, mainEvents } = require('./event-types');
 
-const { ipcMain } = electron;
+if (process.env.NODE_ENV === 'development') {
+  const { ipcMain } = electron;
 
-const createEventLogger = (label, name) => (event, data = {}) => {
-  log.info(chalk.yellow(label, name));
-  Object.entries(data).forEach(([key, value]) => {
-    console.log(`  ${key}: ${JSON.stringify(value)}`);
+  const createEventLogger = (label, name) => (event, data = {}) => {
+    log.info(chalk.yellow(label, name));
+    Object.entries(data).forEach(([key, value]) => {
+      console.log(`  ${key}: ${JSON.stringify(value)}`);
+    });
+  };
+
+  Object.values(browserEvents).forEach((name) => {
+    ipcMain.on(name, createEventLogger('Browser → Main', name));
   });
-};
 
-Object.values(browserEvents).forEach((name) => {
-  ipcMain.on(name, createEventLogger('Browser → Main', name));
-});
-
-Object.values(mainEvents).forEach((name) => {
-  ipcMain.on(name, createEventLogger('Main → Browser', name));
-});
+  Object.values(mainEvents).forEach((name) => {
+    ipcMain.on(name, createEventLogger('Main → Browser', name));
+  });
+}
 
 require('./main');
