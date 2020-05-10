2793285445cc7f8ae2b9b2b525dbb06c999a4dc5
Jamie Mason <jamie@foldleft.io>
2020-05-10 17:10:29 +0100

    fix(electron): match new electron forge config

--- forge.config.js
+++ forge.config.js
@@ -1,29 +1,19 @@
 module.exports = {
-  make_targets: {
-    darwin: ['zip', 'dmg'],
-    linux: ['deb', 'rpm'],
-  },
-  electronInstallerDMG: {
-    icon: 'assets/icons/mac/astexplorer.icns',
-    name: 'ASTExplorer',
-    overwrite: true,
-  },
-  electronPackagerConfig: {
+  makers: [
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+  ],
+  packagerConfig: {
     icon: 'assets/icons/mac/astexplorer',
     name: 'ASTExplorer',
     overwrite: true,
     ignore: ['/scripts', '/static', '/vendor/astexplorer/website'],
     packageManager: 'yarn',
   },
-  electronInstallerDebian: {
-    productName: 'ASTExplorer',
-  },
   github_repository: {
     owner: 'JamieMason',
     name: 'astexplorer.app',
-  },
-  windowsStoreConfig: {
-    packageName: '',
-    name: 'ASTExplorer',
   },
 };
