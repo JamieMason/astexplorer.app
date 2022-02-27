25cb8d282423fbcd27a68db7ebe56b7a1433aa0e
Jamie Mason <jamie@foldleft.io>
2022-02-27 13:36:54 +0000

    fix(redux): remove circular refs from actions

--- src/browser/store.js
+++ src/browser/store.js
@@ -5,12 +5,43 @@ let store = {
   dispatch() {},
 };
 
+const withoutCircularRefs = (action) => {
+  if (action.parser) {
+    return {
+      type: action.type,
+      parser: {
+        category: {
+          ...action.parser.category,
+          parsers: [],
+          transformers: [],
+        },
+      },
+    };
+  }
+  if (action.transformer) {
+    return {
+      type: action.type,
+      transformer: {
+        category: {
+          ...action.transformer,
+          loadTransformer: '<<function>>',
+          transform: '<<function>>',
+        },
+      },
+    };
+  }
+  return action;
+};
+
 window.__AST_EXPLORER_APP_MIDDLEWARE__ = (_store) => {
   store = _store;
   ipcRenderer.send(browserEvents.REDUX_STORE_CREATED);
   return (next) => (action) => {
     if (action.origin !== 'main') {
-      ipcRenderer.send(browserEvents.REDUX_ACTION_DISPATCHED, action);
+      ipcRenderer.send(
+        browserEvents.REDUX_ACTION_DISPATCHED,
+        withoutCircularRefs(action),
+      );
     }
     return next(action);
   };
